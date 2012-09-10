aee097a5a13213e75f8886f4314417631a34282b
sushuang0322@gmail.com <sushuang0322@gmail.com@71b91061-d656-eb1e-ab1f-c8634194c313>
2012-09-10 16:50:44 +0000

    update

--- trunk/zssex-r/zssex/web/js/zssex/JsChart.js
+++ trunk/zssex-r/zssex/web/js/zssex/JsChart.js
@@ -37,11 +37,12 @@
             return this._chartId;
         },
 
-        /**
-         * 初始化chart
-         */
         setChartModel : function(chartModel) {
             this._chartModel = chartModel;
+        },
+        
+        getChartModel: function() {
+            return this._chartModel;
         },
 
         /* draw js chart */
@@ -49,27 +50,13 @@
             this.$$renderChart();
         },
         
-        /**
-         * 渲染chart
-         * @private
-         */
-        $$renderChart : function() {
-            var options, chartModel, chartId, chartType, series, categories, csDatasource, viewParam;
+        $$setupBaseOptions : function() {
+            var chartModel = this.getChartModel(),
+                chartType = chartModel.chartType,
+                viewParam = chartModel.viewParam,
+                chartId = this.getChartId();
             
-            chartModel = this._chartModel;
-            if (!this._chartModel || !chartModel.categories || !chartModel.series
-                    || !chartModel.datasource) {
-                return;
-            }
-            
-            chartType = chartModel.chartType;
-            series = chartModel.series;
-            categories = chartModel.categories;
-            csDatasource = chartModel.csDatasource;
-            viewParam = chartModel.viewParam;
-            chartId = this.getChartId();
-            
-            options = {
+            return {
                 chart : {
                     renderTo : chartId,
                     zoomType : 'x',
@@ -77,238 +64,131 @@
                     width: viewParam.intWidth,
                     height: viewParam.intHeight
                 },
-                credits : {
-                    enabled : false
-                },
+                credits : { enabled : false },
                 xAxis : {
                     gridLineWidth : 1,
                     gridLineColor : '#DBDBDB',
                     showLastLabel : true,
                     tickPosition : 'inside',
-                    // tickPositioner : CHART_TICK_POSITIONER,
                     title : null
                 },
                 yAxis : {
                     gridLineWidth : 1,
                     gridLineColor : '#DBDBDB',
                     lineWidth : 1,
-                    labels : {
-                        align : 'right',
-                        x : -20
-                    }
+                    labels : { align : 'right', x : -20 }
                 },
-                legend : {
-                    enabled : true
-                },
-                title : {
-                    text : null
-                }
-            };
-            
-            options.series = this.$$setupSeries(chartModel);
-
-            /* 不同图表类型 */
-            if (chartType == 'column' || chartType == 'bar') {
-                options.chart.type = 'column';
-                this._chart = new Highcharts.Chart(options);
-            } else if (chartType == 'line') {
-                options.chart.type = 'line';
-                this._chart = new Highcharts.Chart(options);
-            } else if (chartType == 'pie') {
-                options.chart.type = 'pie';
-                this._chart = new Highcharts.Chart(options);
-            } else if (chartType == 'scatter') {
-                options.chart.type = 'scatter';
-                this._chart = new Highcharts.Chart(options);
+                title : { text : viewParam.chartTitle || null }
+            };            
+        },
+        
+        $$setupLegend : function(options) {
+            var series = chartModel.clientModel.series;
+            if series && series.length > 0) {
+                options.legend = {enable: true}; 
+            } else {
+                options.legend = {enable: false};
             }
         },
         
-        $$setupSeries : function(chartModel) {
-             var res = [], ser, i, o, j, item, seriesData, 
-                 chartType = chartModel.chartType,
-                 series = chartModel.series,
-                 categories = chartModel.categories,
-                 sDatasource = chartModel.sDatasource,
-                 csDatasource = chartModel.csDatasource;
-             
-             if (chartType == 'scatter') {
-                 series = series || [];
-                 sDatasource = sDatasource || [];
-                 
-                 for (i = 0; item = series[i]; i++) {
-                     seriesData = sDatasource[item];
-                     ser = {data: []};
-                     ser.name = item || '';
-                     for (j = 0; o = seriesData[j]; j++) {
-                         if (o['y'] > 0) // TEMP FIXME
-                         ser.data.push([o['x'], o['y']]);
-                     }
-                     res.push(ser);
-                 }
-                 
-             } else {
-             
-                 series = series || [];
-                 csDatasource = csDatasource || [];
-    
-                 for (i = 0; item = series[i]; i++) {
-                     ser = {data: []};
-    
-                     ser.name = item || '';
-    //                 ser.yAxis = item.axis || 0;
-    //                 ser.color = item.color || null;
-    //                 ser.format = item.format || null;
-    //                 item.id !== undefined && item.id !== null && (ser.id = item.id);
-    
-                     for (j = 0; o = csDatasource[j]; j++) {
-                         ser.data.push([o['c'], o[item]]);
-                     }
-                     res.push(ser);
-                 }
-           }
-           return res;
-        }
         
+        /**
+         * 设置series
+         */
+        $$setupSeries : function(options) {
+             var ser, i, o, j, item, seriesData, 
+                 chartModel = this.getChartModel(),
+                 chartType = chartModel.chartType,
+                 series = chartModel.clientModel.series,
+                 categories = chartModel.clientModel.categories,
+                 datasource = chartModel.clientModel.datasource;
+             
+             options.series = options.series || [];
+             
+             switch (chartType) {
+                 case 'scatter' : 
+                     series = series || [];
+                     datasource = datasource || {};
+                     
+                     for (i = 0; item = series[i]; i++) {
+                         seriesData = datasource[item];
+                         ser = {data: []};
+                         ser.name = item || '';
+                         for (j = 0; o = seriesData[j]; j++) {
+                             ser.data.push([o['x'], o['y']]);
+                         }
+                         options.series.push(ser);
+                     }
+                     break;
+                     
+                 case 'line':
+                 case 'column':
+                 case 'bar':
+                 case 'pie':
+                     series = series || [];
+                     datasource = datasource || [];
+                     
+                     for (i = 0; item = series[i]; i++) {
+                         ser = {data: []};
+                         
+                         ser.name = item || '';
+                         //                 ser.yAxis = item.axis || 0;
+                         //                 ser.color = item.color || null;
+                         //                 ser.format = item.format || null;
+                         //                 item.id !== undefined && item.id !== null && (ser.id = item.id);
+                         
+                         for (j = 0; o = datasource[j]; j++) {
+                             ser.data.push([o['c'], o[item]]);
+                         }
+                         options.series.push(ser);
+                     }
+             }
+             
+             return options;
+        },    
+        
+        $$createChart : function(options) {
+            var chartType = this.getChartModel().chartType;
             
+            switch (chartType) {
+                case 'line':
+                case 'column': 
+                case 'bar': 
+                    options.chart.type = 'column';
+                    this._chart = new Highcharts.Chart(options);
+                    break;
+                case 'pie': 
+                    options.chart.type = 'pie';
+                    this._chart = new Highcharts.Chart(options);
+                    break;
+                case 'scatter': 
+                    options.chart.type = 'scatter';
+                    this._chart = new Highcharts.Chart(options);
+                    break;
+            }
+            
+            return options;
+        },
+        
+        
+        /**
+         * 渲染chart
+         * @private
+         */
+        $$renderChart : function() {
+            var options, chartModel;
+            
+            chartModel = this.getChartModel();
+            if (!chartModel || !chartModel.clientModel) { // check
+                return;
+            }
+            
+            options = this.$$setupBaseOptions();
+            this.$$setupSeries(options);
+            this.$$setupLegend(options);
+            this.$$createChart(options);
+        }
             
     });
-
-    /** ************************************************************************* */
-
-
-    function $$rrrrenderChart(chartModel) {
-        var options = this.$initOptions();
-
-        $$createChart(options);
-    }
-
-    function $$createChart(options) {
-        var chart;
-        if (options.chartType == 'line') {
-            chart = new STOCKCHART(options);
-        } else if (options.chartType == 'column') {
-            chart = new STOCKCHART(options);
-        }
-        return chart;
-    }
-
-    /**
-     * 构建图表参数
-     * 
-     * @private
-     */
-    function $$initOptions(chartModel) {
-        var options, i, item, datasource, field, me = this;
-
-        options = {
-            chart : {
-                renderTo : target,
-                zoomType : 'x',
-                marginRight : CHART_MARGIN_RIGHT
-            },
-            credits : {
-                enabled : false
-            },
-            xAxis : {
-                gridLineWidth : 1,
-                gridLineColor : '#DBDBDB',
-                showLastLabel : true,
-                tickPosition : 'inside',
-                tickPositioner : CHART_TICK_POSITIONER,
-                // events: {
-                // setExtremes: SETUP_CHART_SETEXTREMES_HANDLER(this)
-                // },
-                title : null
-            },
-            yAxis : {
-                gridLineWidth : 1,
-                gridLineColor : '#DBDBDB',
-                lineWidth : 1,
-                labels : {
-                    align : 'right',
-                    x : -20
-                },
-                title : null
-
-            },
-            legend : {
-                enabled : false
-            },
-            title : {
-                text : null
-            }
-        };
-
-        if (!dom.ieVersion) {
-            options.plotOptions = {
-                column : {
-                    shadow : true,
-                    borderWidth : 1
-                }
-            };
-        }
-
-        datasource = $$setupChartData(chartModel.tickType, chartModel.field,
-                chartModel.chartData);
-
-        options.series = SETUP_SERIES(this._aSeries, this._oCategory.field,
-                datasource);
-        options.xAxis = extend(options.xAxis, SETUP_CATEGORY(this._oCategory));
-        options.yAxis = extend(options.yAxis, SETUP_AXIS(this._aAxis));
-        options.rangeSelector = SETUP_RANGE_SELECTOR(this._oCategory);
-        SETUP_NAVIGATOR(options, this._oCategory);
-
-        options.tooltip = SETUP_TOOLTIP(this._oCategory.type, moreInfo,
-                holidayInfo, this._oCategory.tipCallback);
-
-        return options;
-    }
-
-    /**
-     * @private
-     */
-    function $$setupChartData(type, field, data) {
-        var res = [], i, item, o;
-
-        for (i = 0; item = data[i]; i++) {
-            o = item; // FIXME
-            if (PARSE_DATE_HANDLER[type]) {
-                o[field] = PARSE_DATE_HANDLER[type].call(null, o[field]);
-            }
-            res.push(o);
-        }
-        return res;
-    }
-
-    var PARSE_DATE_HANDLER = {
-        'datetime' : function(data) {
-            if (typeof data == 'string') {
-                if (data.indexOf('-') >= 0) {
-                    data = data.split('-');
-                    return new Date(parseInt(data[0], 10),
-                            parseInt(data[1], 10) - 1, parseInt(data[2], 10))
-                            .getTime();
-                } else {
-                    return parseInt(data, 10);
-                }
-            }
-            return data;
-        },
-        'month' : function(data) {
-            data = data.split('-');
-            return new Date(parseInt(data[0], 10), parseInt(data[1], 10) - 1, 1)
-                    .getTime();
-        },
-        'quarter' : function(data) {
-            var par = [ 0, 0, 3, 6, 9 ];
-
-            data = data.split('-Q');
-            return new Date(parseInt(data[0], 10), par[parseInt(data[1], 10)],
-                    1).getTime();
-        },
-        'year' : function(data) {
-            return new Date(parseInt(data, 10), 0, 1).getTime();
-        }
-    };
 
 })();
