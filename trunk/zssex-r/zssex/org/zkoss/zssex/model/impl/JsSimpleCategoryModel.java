996cf1c126859562a21379b9f92341afbd445e72
sushuang0322@gmail.com <sushuang0322@gmail.com@71b91061-d656-eb1e-ab1f-c8634194c313>
2012-09-12 02:25:41 +0000

    update

--- trunk/zssex-r/zssex/org/zkoss/zssex/model/impl/JsSimpleCategoryModel.java
+++ trunk/zssex-r/zssex/org/zkoss/zssex/model/impl/JsSimpleCategoryModel.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.zkoss.zssex.model.JsChartModel;
+import org.zkoss.zssex.util.ChartUtil;
+import org.zkoss.zssex.util.ChartUtil.DataFormatAnalyzer;
+import org.zkoss.zssex.util.ChartUtil.TickTypeAnalyzer;
+import org.zkoss.zssex.util.CommonUtil;
 import org.zkoss.zul.SimpleCategoryModel;
 
 public class JsSimpleCategoryModel extends SimpleCategoryModel implements JsChartModel {
@@ -16,31 +20,39 @@ public class JsSimpleCategoryModel extends SimpleCategoryModel implements JsChar
 
 		Map<String, Object> clientModel = new HashMap<String, Object>();
 
-		List<Map<String, Object>> categoryDatasource = new ArrayList<Map<String, Object>>();
-		this.fillClientDatasource(categoryDatasource);
+		this.fillClientDatasource(clientModel);
 		
-		clientModel.put("categories", this.getCategories());
-		clientModel.put("series", this.getSeries());
-		clientModel.put("datasource", categoryDatasource);
+		clientModel.put(CATEGORIES, CommonUtil.wrapCollection(this.getCategories(), LABEL));
+		/** 目前暂所有都是category */
+		clientModel.put(TICK_TYPE, TICK_TYPE_CATEGORY);
+//		clientModel.put(TICK_TYPE, ChartUtil.analyzeValues(this.getCategories(), TickTypeAnalyzer.class));
+		clientModel.put(SERIES, CommonUtil.wrapCollection(this.getSeries(), LABEL));
 		
 		return clientModel;
 	}
 
-	private void fillClientDatasource(List<Map<String, Object>> categoryDatasource) {
+	private void fillClientDatasource(Map<String, Object> clientModel) {
 		
+		List<Map<String, Object>> categoryDatasource = new ArrayList<Map<String, Object>>();
 		List<Comparable<?>> categories = (List<Comparable<?>>) this.getCategories();
 		List<Comparable<?>> series = (List<Comparable<?>>) this.getSeries();
+		DataFormatAnalyzer analyzer = new DataFormatAnalyzer();
 		
 		for (int j = 0; j < categories.size(); j++) {
 			Map<String, Object> csDataMap = new HashMap<String, Object>();
-			csDataMap.put("c", categories.get(j));
+			csDataMap.put(CATEGORIES_ITEM, categories.get(j));
 			for (int i = 0; i < series.size(); i++) {
 				Number number = this.getValue(series.get(i), categories.get(j));
 				if (number != null) {
 					csDataMap.put(series.get(i).toString(), number);
+					analyzer.analyze(number);
 				}
 			}
 			categoryDatasource.add(csDataMap);
 		}
+		
+		clientModel.put(DATASOURCE, categoryDatasource);
+		clientModel.put(SERIES_FORMAT, analyzer.getResult());
 	}
+
 }
